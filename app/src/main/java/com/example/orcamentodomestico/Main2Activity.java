d3d5ee10ab53dac8ac1721c5aa3fd4fc9aa55f59
Mauro4879 <mauro.guterres@alcidesmaya.edu.br>
2020-07-04 14:43:37 -0300

    Funcionando com banco

--- app/src/main/java/com/example/orcamentodomestico/Main2Activity.java
+++ app/src/main/java/com/example/orcamentodomestico/Main2Activity.java
@@ -18,6 +18,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -25,6 +27,8 @@ import java.util.Locale;
 
 public class Main2Activity extends AppCompatActivity {
 
+
+    DatabaseReference dbreference;
     // criação do adapter e da lista
     ListAdapterItem adapter;
     ArrayList<Item> listaItens;
@@ -54,6 +58,8 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        dbreference = FirebaseDatabase.getInstance().getReference("relatorios");
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
@@ -172,12 +178,15 @@ public class Main2Activity extends AppCompatActivity {
 
                 for (Iterator<Item> iterator = listaItens.iterator(); iterator.hasNext(); ) {
                     Item item = iterator.next(); // pega o item da lista
-
+                    String id = dbreference.push().getKey();
+                    dbreference.child(id).setValue(item);
                     pegaValor = Float.parseFloat(item.getValor().replaceAll("\\D", ""));
                     despesas = (despesas) + (pegaValor / 100);
                 }
                 Intent i = new Intent(Main2Activity.this, Main4Activity.class);
                 i.putExtra("TotalDespesas", despesas);
+                //i.putExtra("DespesasDiscriminadas",listaItens);
+
                 startActivity(i);
 
             } catch (NumberFormatException e) {
